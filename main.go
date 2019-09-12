d6ea036cc4385808f808d7cba95af364da75f7eb
ogrew <okawara-ayato@kayac.com>
2019-09-13 00:14:09 +0900

    read num from stdin

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
 func main() {
+	var src []byte
+	// 標準入力を受け取る
+	src, _ = ioutil.ReadAll(os.Stdin)
+	input := string(src)
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("  .global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("  movq $42, %%rax\n")
+	fmt.Printf("  movq $%d, %%rax\n", num)
 	fmt.Printf("  ret\n")
 }
